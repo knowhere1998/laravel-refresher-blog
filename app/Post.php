e28050b209b57153f9bae23fa7344ec49181c3e6
knowhere1998 <darekar.amey@gmail.com>
2019-07-21 12:40:13 +0530

    Added relationship between post and user as author

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+	protected $fillable = [
+		'author_id',
+		'title',
+		'content',
+		'posted_at'
+	];
+
+	public $dates = [ 'posted_at' ];
+
+	public function author() {
+		return $this->belongsTo('App\User', 'author_id');
+	}
 }
