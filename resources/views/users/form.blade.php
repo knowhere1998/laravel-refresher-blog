c9376c939e4ee0b337eb0d3713f9abf1069902db
knowhere1998 <darekar.amey@gmail.com>
2019-07-27 15:04:17 +0530

    Added Profile Edit Form

---
+++ resources/views/users/form.blade.php
@@ -0,0 +1,28 @@
+{!! Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user]]) !!}
+
+<div class="form-group">
+	{!! Form::label('name', 'Name') !!}
+	{!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('email', 'Email') !!}
+	{!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('password', 'Password') !!}
+	{!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('password_confirmation', "Confirm Password") !!}
+	{!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Confirm Password']) !!}
+</div>
+
+<div class="float-right">
+	<a href="{{ route('users.show', $user) }}" class="btn btn-default">{{ 'back' }}</a>
+	{!! Form::submit('save', ['class' => 'btn btn-success']) !!}
+</div>
+
+{!! Form::close() !!}
