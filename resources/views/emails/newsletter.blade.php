5907992ebc653f30d7817df3810e6ff8fab8d27a
knowhere1998 <darekar.amey@gmail.com>
2019-09-26 21:12:31 +0530

    Added Newsletter view

---
+++ resources/views/emails/newsletter.blade.php
@@ -0,0 +1,16 @@
+<h1> Welcome to the newsletter of the month! </h1>
+
+<p>
+	Here is the list of the last {{ $posts->count() }} articles published during the month:
+</p>
+
+<ul>
+	@foreach($posts as $post)
+		<li>{{ link_to_route('posts.show', $post->title, $post) }}</li>
+	@endforeach
+</ul>
+
+<p>
+	Thank you for subscribing to our newsletter
+	Learn to pronounce
+</p>
--- resources/views/layouts/newsletter-form.blade.php
+++ resources/views/layouts/newsletter-form.blade.php
@@ -1,11 +1,11 @@
 <div class="container">
-	{!! Form::open(['route' => 'newsletter-subscriptions.store', 'method' => 'post', 'class' => 'navbar-form navbar-right']) !!}
+	{!! Form::open(['route' => 'emails', 'method' => 'post', 'class' => 'navbar-form navbar-right']) !!}
 	<div class="form-row">
 		<div class="col-md-4 offset-md-3">
 			{!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => "Enter your email address"]) !!}
 		</div>
 		<div class="col-md-2">
-			{!! Form::submit("Subscribe to our newsletter", ['class' => 'btn btn-primary']) !!}
+			{!! Form::submit("emails", ['class' => 'btn btn-primary']) !!}
 		</div>
 	</div>
 	{!! Form::close() !!}
