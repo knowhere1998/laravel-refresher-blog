4fd370aa268bbf79120dfc30cbfa35036f6a6fb2
knowhere1998 <darekar.amey@gmail.com>
2019-09-26 22:43:22 +0530

    added ModelFactory calls in DatabaseSeeder->run() method

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Comment;
+use App\NewsletterSubscription;
+use App\Post;
+use App\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -9,8 +14,22 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+		$faker = Factory::create();
+		factory(User::class, 10)
+			->create()
+			->each(function ($user) use ($faker) {
+				factory(Post::class, $faker->numberBetween(2,20))
+					->create([
+						'author_id' => $user->id
+					])
+					->each(function ($post) use ($faker) {
+						factory(Comment::class, $faker->numberBetween(10,60))
+							->create([
+								'post_id' => $post->id
+							]);
+					});
+			});
+		factory(NewsletterSubscription::class, $faker->numberBetween(10, 50))->create();
     }
 }
